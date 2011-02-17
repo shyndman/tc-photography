9a00e10ccff9a7d7199ac0415adb1624cdb2113f
shyndman <scotty.hyndman@gmail.com>
2011-02-16 22:19:53 -0500

    intro page complete gallery images added nav script written menu intro script written

--- js/script.js
+++ js/script.js
@@ -1,26 +1,22 @@
-/* Author: 
+/* Author: Scott Hyndman */
 
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+$(function() {
+	// menu fade-in
+	// $("nav").removeClass("visuallyhidden").hide().delay(1000).fadeIn(1000);
+	$("nav").removeClass("visuallyhidden"); //! DEBUG
+	
+	// navigation
+	var lastNav = null;
+	$("nav a").click(function(evt) {
+		if (lastNav) {
+			lastNav.removeClass("selected");
+			$("#" + lastNav.data("page")).hide();
+		} else {
+			$("#intro").hide();
+		}
+		var nav = lastNav = $(this).addClass("selected");
+		$("#" + nav.data("page")).show();
+		
+		return false;
+	});
+});
